c3474ebd83e074fc8cbe598bc11bf6a93f3430ad
LJ Web Development <ljwebdevelopmentok@gmail.com>
2026-06-01 00:57:42 -0500

    Fix GitHub Pages base path

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/chore-tracker">
       <App />
     </BrowserRouter>
   </StrictMode>,
